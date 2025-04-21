9287e0d986ac19cc7f5293999e83a41b0ee00998
Lauren Farrell <150389282+BUFFALOLO@users.noreply.github.com>
2025-04-21 16:11:33 +0000

    ready to submit

---
+++ src/setupTests.js
@@ -0,0 +1,19 @@
+djest.mock('firebase/app', () => {
+  return {
+    initializeApp: jest.fn(() => ({})),
+  };
+});
+
+jest.mock('firebase/auth', () => {
+  return {
+    getAuth: jest.fn(() => ({})),
+  };
+});
+
+jest.mock('firebase/firestore', () => {
+  return {
+    getFirestore: jest.fn(() => ({})),
+    collection: jest.fn(),
+    addDoc: jest.fn(() => Promise.resolve()),
+  };
+});
